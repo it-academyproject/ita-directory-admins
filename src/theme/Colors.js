92e588d32cfe9dc68a01d89e73557c75b6bb5a64
ingridinara <ingridinara@me.com>
2021-01-26 10:38:12 +0100

    new footer and revised login - second input

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,5 +1,14 @@
 const Colors = {
-	white: "#FFFFFF",
-	black: "#000000",
+	whiteColor: "#FFFFFF",
+	blackColor: "#000000",
+
+	// Buttons
+	lightPrimary: "rgba(70, 193, 247, 1)",
+	darkPrimary: "rgba(0, 115, 230, 1)",
+	lightGreen: "rgba(109, 211, 100, 1)",
+	darkGreen: "rgba(36, 154, 54, 1)",
+	darkBlueColor: "rgba(7, 76, 132, 1)",
+	redColor: "rgba(193, 12, 12, 1)",
+	greyColor: "rgba(80, 77, 84, 1)",
 };
 export default Colors;
